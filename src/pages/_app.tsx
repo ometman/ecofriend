3d4d917b9be46ba77e4c138ea042a2885026a2e0
ometman <revomet@gmail.com>
2024-07-10 22:10:57 +0000

    Add NavBar to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 // src/pages/_app.tsx
+import NavBar from '@/components/NavBar';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <NavBar />
       <Component {...pageProps} />
     </>
   );

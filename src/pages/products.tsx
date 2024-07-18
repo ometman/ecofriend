6d98544f41bb30c9bd17af22ec27cf54489e592e
ometman <revomet@gmail.com>
2024-07-18 10:04:39 +0000

    Product: refactor products page with link to product details

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Product } from './services/productData';
-
 import styles from '../styles/Products.module.css';
+import Link from 'next/link';
 
 const Products: React.FC = () => {
     const [products, setProducts]  = useState<Product[]>([])
@@ -38,23 +38,29 @@ const Products: React.FC = () => {
     if (error) {
         return <div>Error: {error}</div>;
     }
+
+    if (!products) {
+        return <div>No product found</div>;
+    }
    
     return (
         <div className={styles.container}>
             <h1>Eco-Friendly Products</h1>
             <ul className={styles.productList}>
                 {products.map(product => (
-                    <li key={product.id} className={styles.productItem}>
-                        <h2><strong>Name: </strong>{product.name}</h2>
-                        <p><strong>Brand: </strong>{product.brand}</p>
-                        <p><strong>Category: </strong>{product.category}</p>
-                        <p><strong>Description: </strong>{product.description}</p>
-                        <p><strong>ecoRating: </strong>{product.ecoRating}</p>
-                        <p>{product.imageUrl && (
-                            <img src={product.imageUrl} alt={product.name} className="styles.productImage" />
-                        )}
-                        </p>
-                    </li>
+                    <Link href={`/products/${product.id}`}>
+                        <li key={product.id} className={styles.productItem}>
+                            <h2><strong>Name: </strong>{product.name}</h2>
+                            <p><strong>Brand: </strong>{product.brand}</p>
+                            <p><strong>Category: </strong>{product.category}</p>
+                            <p><strong>Description: </strong>{product.description}</p>
+                            <p><strong>ecoRating: </strong>{product.ecoRating}</p>
+                            <p>{product.imageUrl && (
+                                <img src={product.imageUrl} alt={product.name} className="styles.productImage" />
+                            )}
+                            </p>
+                        </li>
+                    </Link>
                 ))}
             </ul>       
         </div>

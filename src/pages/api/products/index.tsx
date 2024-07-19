539a4c13061a2f343b70de3f0d675ce2f8aa5053
ometman <revomet@gmail.com>
2024-07-19 14:41:06 +0000

    Product: Refactor the seed data and number of products

--- src/pages/api/products/index.tsx
+++ src/pages/api/products/index.tsx
@@ -5,7 +5,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'GET') {
         try {
             const products = await getProducts();
-            console.log("products")
             res.status(200).json(products);
         } catch (error) {
             res.status(500).json({ error: 'Failed to fetch products' });
